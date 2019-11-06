d4449db7df0bcc9aa426bc7e9a95137ea30fc7ee
kanopa <konoplyanka1999@gmail.com>
2019-11-06 22:06:14 +0200

    add new-note

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewNoteComponent } from './new-note/new-note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewNoteComponent
   ],
   imports: [
     BrowserModule,
