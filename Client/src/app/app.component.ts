3c9890e9b75bcc9627588388b9708e3414b13a66
Davyd Konopatskyi <40700475+DavidQ7@users.noreply.github.com>
2019-11-07 22:21:37 +0200

    add adaptability to side-bar

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Client';
-  activeMenu = true;
 
+  activeMenu = false;
+
+  constructor() {
+
+  }
+  ngOnInit() {
+  }
   DropMenu() {
     this.activeMenu = !this.activeMenu;
+
+    const menu = document.getElementsByTagName('aside')[0] as HTMLElement;
+    if (this.activeMenu === true) {
+    menu.style.display = 'block';
+    } else {
+      menu.style.display = 'none';
+    }
   }
 }
