c8e0f7e664a8e5a43e409c0dff77ab287dfb8349
Davyd Konopatskyi <40700475+DavidQ7@users.noreply.github.com>
2019-11-07 23:54:29 +0200

    add routing

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NootsComponent } from './noots/noots.component';
+import { InvitesComponent } from './invites/invites.component';
+import { PeopleComponent } from './people/people.component';
+import { LabelsComponent } from './labels/labels.component';
+import { NotesComponent } from './notes/notes.component';
+import { GroupsComponent } from './groups/groups.component';
+import { BinComponent } from './bin/bin.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: NootsComponent},
+  {path: 'noots', component: NootsComponent},
+  {path: 'notes', component: NotesComponent},
+  {path: 'people', component: PeopleComponent},
+  {path: 'groups', component: GroupsComponent},
+  {path: 'labels', component: LabelsComponent},
+  {path: 'invites', component: InvitesComponent},
+  {path: 'bin', component: BinComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
