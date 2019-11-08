4a6c9a652d47be9001e4d81dcddad8c9198e18d6
kanopa <konoplyanka1999@gmail.com>
2019-11-09 01:51:29 +0200

    add wrapp label

--- Client/src/app/view-label/view-label.component.ts
+++ Client/src/app/view-label/view-label.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgModule } from '@angular/core';
+import {animate, transition, trigger, state, style} from '@angular/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+@NgModule({ imports: [BrowserAnimationsModule]})
 @Component({
   selector: 'app-view-label',
   templateUrl: './view-label.component.html',
-  styleUrls: ['./view-label.component.sass']
+  styleUrls: ['./view-label.component.sass'],
+/*animations: [
+    trigger('AddAnimate', [
+      transition('initial=>final', [
+        animate('1s')
+      ]),
+      transition('final=>initial', [
+        animate('1s')
+      ]),
+  ]),
+  ]*/
 })
 export class ViewLabelComponent implements OnInit {
 
   constructor() { }
-
+  // currentState = 'initial';
+  update = false;
+  update1 = false;
+  update22() {
+    // this.currentState = this.currentState === 'initial' ? 'final' : 'initial';
+  }
   ngOnInit() {
   }
 
