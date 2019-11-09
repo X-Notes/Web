1ee9b72bc195cd1c3301a327aee3bd44f0741b52
kanopa <konoplyanka1999@gmail.com>
2019-11-09 07:58:21 +0200

    add some logic

--- Client/src/app/view-label/view-label.component.ts
+++ Client/src/app/view-label/view-label.component.ts
@@ -22,7 +22,30 @@ export class ViewLabelComponent implements OnInit {
   constructor() { }
   // currentState = 'initial';
   update = false;
-  update1 = false;
+  h1color = '#DDFFCD';
+  changeColor(id) {
+    if ('firstLi' === id) {
+      this.h1color = '#FFCDCD';
+    } else if ('secondLi' === id) {
+      this.h1color = '#FFEBCD';
+    } else if ('thirdLi' === id) {
+      this.h1color = '#FFFDCD';
+    } else if ('fourthLi' === id) {
+      this.h1color = '#DDFFCD';
+    } else if ('fifthLi' === id) {
+      this.h1color = '#CDFFD8';
+    } else if ('sixLi' === id) {
+      this.h1color = '#CDFFFC';
+    } else if ('sevenLi' === id) {
+      this.h1color = '#CDEEFF';
+    } else if ('eithLi' === id) {
+      this.h1color = '#CDD5FF';
+    } else if ('nineLi' === id) {
+      this.h1color = '#EFCDFF';
+    } else if ('tenLi' === id) {
+      this.h1color = '#FFCDF4';
+    }
+ }
   update22() {
     // this.currentState = this.currentState === 'initial' ? 'final' : 'initial';
   }
