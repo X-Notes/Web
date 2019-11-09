2116619004ea086c934c9633d86dd2866ed6db8c
kanopa <konoplyanka1999@gmail.com>
2019-11-09 19:45:04 +0200

    pull dev and fix conflict

--- Client/src/app/Labels/labels/labels.component.ts
+++ Client/src/app/Labels/labels/labels.component.ts
@@ -8,7 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class LabelsComponent implements OnInit {
 
   constructor() { }
-
+  // currentState = 'initial';
+  update = false;
+  h1color = '#DDFFCD';
+  changeColor(id) {
+    if ('firstLi' === id) {
+      this.h1color = '#FFCDCD';
+    } else if ('secondLi' === id) {
+      this.h1color = '#FFEBCD';
+    } else if ('thirdLi' === id) {
+      this.h1color = '#FFFDCD';
+    } else if ('fourthLi' === id) {
+      this.h1color = '#DDFFCD';
+    } else if ('fifthLi' === id) {
+      this.h1color = '#CDFFD8';
+    } else if ('sixLi' === id) {
+      this.h1color = '#CDFFFC';
+    } else if ('sevenLi' === id) {
+      this.h1color = '#CDEEFF';
+    } else if ('eithLi' === id) {
+      this.h1color = '#CDD5FF';
+    } else if ('nineLi' === id) {
+      this.h1color = '#EFCDFF';
+    } else if ('tenLi' === id) {
+      this.h1color = '#FFCDF4';
+    }
+ }
+  update22() {
+    // this.currentState = this.currentState === 'initial' ? 'final' : 'initial';
+  }
   ngOnInit() {
   }
 
