9a19add95469b20c013ed32bd976ebdd0e53eca4
kanopa <konoplyanka1999@gmail.com>
2019-11-12 14:56:51 +0200

    divider label

--- Client/src/app/Labels/labels.module.ts
+++ Client/src/app/Labels/labels.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LabelsComponent } from './labels/labels.component';
+import { LabelComponent } from './label/label.component';
 
 
 @NgModule({
-  declarations: [LabelsComponent],
+  declarations: [LabelsComponent, LabelComponent],
   imports: [
     CommonModule
   ],
