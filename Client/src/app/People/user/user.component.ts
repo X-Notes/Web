1e882c9985b86cfacc659105ec9bfabd0faff8a6
kanopa <konoplyanka1999@gmail.com>
2019-11-16 10:47:59 +0200

    add carcas skeleton for people

--- Client/src/app/People/user/user.component.ts
+++ Client/src/app/People/user/user.component.ts
@@ -9,7 +9,19 @@ export class UserComponent implements OnInit {
 
   constructor() { }
 
+  element;
+  update = false;
+
+  changeIcon() {
+    this.update = !this.update;
+    if (this.update === true) {
+      this.element.setAttribute('style', 'transform: rotate(180deg)');
+    } else {
+      this.element.setAttribute('style', 'transform: rotate(0deg)');
+    }
+  }
   ngOnInit() {
+    this.element = document.getElementsByClassName('image-options')[0];
   }
 
 }
