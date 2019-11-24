b5202c1f9580a1ba22f3ca39ee5a9754c1fc8606
kanopa <konoplyanka1999@gmail.com>
2019-11-24 20:09:33 +0200

    add carcase highlighting

--- Client/src/app/Noots/noot/noot.component.ts
+++ Client/src/app/Noots/noot/noot.component.ts
@@ -8,11 +8,29 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class NootComponent implements OnInit {
 
   constructor() { }
+
+  acceptCommon = 'assets/highlighting/accept-common.svg';
+  acceptWhite = 'assets/highlighting/accept-white.svg';
   update = true;
+  color = '';
+  colorUpdate = false;
   @Output() Changed = new EventEmitter<boolean>();
+
   change(increased: any) {
       this.Changed.emit(increased);
-      console.log(2, increased);
+  }
+  changeColor() {
+    this.colorUpdate = !this.colorUpdate;
+    if (this.colorUpdate === true) {
+      this.color = 'rgba(101, 226, 113, 0.69)';
+      this.update = false;
+      this.change(this.update);
+      console.log(this.update);
+    } else {
+      this.update = true;
+      this.color = '';
+      this.change(this.update);
+    }
   }
   ngOnInit() {
   }
