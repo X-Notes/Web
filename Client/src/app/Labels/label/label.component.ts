dd6ad3c35721ad81d407f6d823cd6fb5aa502490
Davyd Konopatskyi <40700475+DavidQ7@users.noreply.github.com>
2019-12-16 17:50:16 +0200

    styles

--- Client/src/app/Labels/label/label.component.ts
+++ Client/src/app/Labels/label/label.component.ts
@@ -27,41 +27,13 @@ export class LabelComponent implements OnInit {
   @Input() label: Label;
 
   update = false;
-  h1color = '#DDFFCD';
-  element;
   changeIcon() {
     this.update = !this.update;
-    if (this.update === true) {
-      this.element.setAttribute('style', 'transform: rotate(180deg)');
-    } else {
-      this.element.setAttribute('style', 'transform: rotate(0deg)');
-    }
   }
   changeColor(id) {
-    if ('firstLi' === id) {
-      this.h1color = '#FFCDCD';
-    } else if ('secondLi' === id) {
-      this.h1color = '#FFEBCD';
-    } else if ('thirdLi' === id) {
-      this.h1color = '#FFFDCD';
-    } else if ('fourthLi' === id) {
-      this.h1color = '#DDFFCD';
-    } else if ('fifthLi' === id) {
-      this.h1color = '#CDFFD8';
-    } else if ('sixLi' === id) {
-      this.h1color = '#CDFFFC';
-    } else if ('sevenLi' === id) {
-      this.h1color = '#CDEEFF';
-    } else if ('eithLi' === id) {
-      this.h1color = '#CDD5FF';
-    } else if ('nineLi' === id) {
-      this.h1color = '#EFCDFF';
-    } else if ('tenLi' === id) {
-      this.h1color = '#FFCDF4';
-    }
+    this.label.color = id;
  }
   ngOnInit() {
-    this.element = document.getElementsByClassName('image-options')[0];
   }
 
 }
--- Client/src/app/Labels/labels.module.ts
+++ Client/src/app/Labels/labels.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import {LabelsComponent } from './labels/labels.component';
 import { LabelComponent } from './label/label.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LabelsComponent, LabelComponent],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   exports: [LabelsComponent]
 })
