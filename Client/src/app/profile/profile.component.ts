7ab0a94f67661ea64e1593b7ec27b36a20362816
Davyd Konopatskyi <40700475+DavidQ7@users.noreply.github.com>
2020-01-29 22:43:14 +0200

    profile input

--- Client/src/app/profile/profile.component.ts
+++ Client/src/app/profile/profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../Models/User/User';
+import { UserService } from '../Services/user.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  unsubscribe = new Subject();
+
+  constructor(private router: Router, private userService: UserService) {
+
+  }
 
   ngOnInit() {
+    this.userService.Get()
+    .pipe(takeUntil(this.unsubscribe))
+    .subscribe(user => { this.user = user; }, error => {
+      this.router.navigate(['/about']);
+    });
   }
 
 }
