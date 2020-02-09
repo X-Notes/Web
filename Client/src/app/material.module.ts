11b01356422e4f35d3e7fe98a8a2c072cca67274
Davyd Konopatskyi <40700475+DavidQ7@users.noreply.github.com>
2020-02-09 02:01:29 +0200

    delete cdk

--- Client/src/app/material.module.ts
+++ Client/src/app/material.module.ts
@@ -1,10 +1,8 @@
 import {NgModule} from '@angular/core';
-import {DragDropModule} from '@angular/cdk/drag-drop';
 
 
 @NgModule({
   exports: [
-    DragDropModule,
   ]
 })
 export class MaterialModule { }
