e61de301cbc4938fe069bccd6fc791424ac48cc4
Davyd Konopatskyi <40700475+DavidQ7@users.noreply.github.com>
2020-02-09 16:42:51 +0200

    fix order

--- NootsAPI/Noots.BusinessLogic/Services/NoteService.cs
+++ NootsAPI/Noots.BusinessLogic/Services/NoteService.cs
@@ -23,15 +23,7 @@ namespace Noots.BusinessLogic.Services
 
         public async Task<string> NewNote(string Email)
         {
-            var dbNotes = await noteRepository.GetAll(Email);
-
-            int order = 0;
-            if(dbNotes.Count != 0)
-            {
-                order = dbNotes.Max(x => x.Order);
-                ++order;
-            }
-            var newNote = await noteRepository.New(new Note() { Email = Email, Order = order }) ;
+            var newNote = await noteRepository.New(new Note() { Email = Email }) ;
             return newNote.Id.ToString();
         }
         public async Task<List<DTONote>> GetAll(string email)
