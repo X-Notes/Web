0da3e68139d80632e8c37106089232e4c10a9c88
Davyd Konopatskyi <40700475+DavidQ7@users.noreply.github.com>
2020-02-14 00:49:52 +0200

    d

--- Shared/Shared/DTO/PartDTO/DTOPart.cs
+++ Shared/Shared/DTO/PartDTO/DTOPart.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Shared.DTO.PartDTO
 {
+
     public class DTOPart
     {
         public string Id { set; get; }
