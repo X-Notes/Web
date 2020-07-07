cede79c83868e6d8fabd9c8048fdcfdb8693eda2
kanopa <konoplyanka1999@gmail.com>
2020-07-07 20:21:12 +0300

    add labels

--- Client/src/app/content/labels/labels.module.ts
+++ Client/src/app/content/labels/labels.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LabelsComponent } from './labels/labels.component';
 import { LabelsRouting } from './labels-routing';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LabelComponent } from './label/label.component';
 
 
 
 @NgModule({
-  declarations: [LabelsComponent],
+  declarations: [LabelsComponent, LabelComponent],
   imports: [
     CommonModule,
     LabelsRouting,
