b518c9af5b731ac44af464420db320e0c7049d64
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-07-23 20:58:11 +0300

    add con

--- deprecated/Client/src/environments/environment.prod.ts
+++ deprecated/Client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  nootsAPI : 'http://localhost:5000',
+  nootsAPI : 'http://noots.westeurope.cloudapp.azure.com:5000',
   firebase: {
     apiKey: 'AIzaSyARUCz9Wc864vcTUf8EK51pMH1t9jpFnTc',
     authDomain: 'noots-storm.firebaseapp.com',
