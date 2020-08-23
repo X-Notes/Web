17d40069565d3f1bf2db2162c58700315fd29527
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-08-23 15:42:22 +0300

    folder view

--- Client/src/app/shared/modal_components/change-color/change-color.component.ts
+++ Client/src/app/shared/modal_components/change-color/change-color.component.ts
@@ -50,4 +50,25 @@ export class ChangeColorComponent implements OnInit {
     // await this.store.dispatch(new ChangeColorNote(this.current, NoteType.Private)).toPromise();
     this.dialogRef.close();
   }
+
+  shadeColor(color, percent) {
+
+    let R = parseInt(color.substring(1, 3), 16);
+    let G = parseInt(color.substring(3, 5), 16);
+    let B = parseInt(color.substring(5, 7), 16);
+
+    R = parseInt((R * (100 + percent) / 100).toString(), 10);
+    G = parseInt((G * (100 + percent) / 100).toString(), 10);
+    B = parseInt((B * (100 + percent) / 100).toString(), 10);
+
+    R = (R < 255) ? R : 255;
+    G = (G < 255) ? G : 255;
+    B = (B < 255) ? B : 255;
+
+    const RR = ((R.toString(16).length === 1) ? '0' + R.toString(16) : R.toString(16));
+    const GG = ((G.toString(16).length === 1) ? '0' + G.toString(16) : G.toString(16));
+    const BB = ((B.toString(16).length === 1) ? '0' + B.toString(16) : B.toString(16));
+
+    return '#' + RR + GG + BB;
+  }
 }
