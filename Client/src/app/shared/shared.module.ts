1a8c2f962d38825bdb69b61f0ca5be3785da7e7c
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-08-25 19:06:55 +0300

    pipe

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -8,11 +8,12 @@ import { DialogService } from './modal_components/dialog.service';
 import { ChangeColorComponent } from './modal_components/change-color/change-color.component';
 import { EditingLabelsNoteComponent } from './modal_components/editing-labels-note/editing-labels-note.component';
 import { HammerModule } from '@angular/platform-browser';
+import { ReplacePipe } from './pipes/replace.pipe';
 
 @NgModule({
-  declarations: [ChangeColorComponent, EditingLabelsNoteComponent],
+  declarations: [ChangeColorComponent, EditingLabelsNoteComponent, ReplacePipe],
   imports: [CommonModule , TranslateModule, SimplebarAngularModule, MatDialogModule, HammerModule],
-  exports: [TranslateModule, MatRippleModule, SimplebarAngularModule, MatDialogModule, HammerModule],
+  exports: [TranslateModule, MatRippleModule, SimplebarAngularModule, MatDialogModule, HammerModule, ReplacePipe],
   providers: [DialogService]
 })
 export class SharedModule { }
