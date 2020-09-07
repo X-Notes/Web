996835e193838dafe558389d5930ea7af8b674c0
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-09-07 19:20:29 +0300

    fix bug with language

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,24 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { PersonalizationService } from './shared/services/personalization.service';
 import { SignalRService } from './core/signal-r.service';
 import { Store } from '@ngxs/store';
 import { UserStore } from './core/stateUser/user-state';
 import { Language } from './shared/enums/Language';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+
+  destroy = new Subject<void>();
 
   constructor(private translateService: TranslateService, private store: Store) {
   }
 
-  async ngOnInit() {
-    const lang  = this.store.selectSnapshot(UserStore.getUserLanguage);
-    await this.translateService.use(lang).toPromise();
+  ngOnInit() {
+    this.store
+      .select(UserStore.getUserLanguage)
+      .pipe(takeUntil(this.destroy))
+      .subscribe(async (lang) => {
+        if (lang) {
+          await this.translateService.use(lang).toPromise();
+        } else {
+          await this.translateService.use(Language.EN).toPromise();
+        }
+      });
+  }
+
+
+  ngOnDestroy(): void {
+    this.destroy.next();
+    this.destroy.complete();
   }
 
 }
