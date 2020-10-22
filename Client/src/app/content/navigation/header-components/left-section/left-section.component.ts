4df29b1f94ce53a549da17362e77753d01a42a83
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-10-22 21:50:47 +0300

    init left section

---
+++ Client/src/app/content/navigation/header-components/left-section/left-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-section',
+  templateUrl: './left-section.component.html',
+  styleUrls: ['./left-section.component.scss']
+})
+export class LeftSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Client/src/app/content/navigation/navigation.module.ts
+++ Client/src/app/content/navigation/navigation.module.ts
@@ -6,10 +6,11 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MenuComponent } from './menu/menu.component';
 import { ProfileSectionComponent } from './header-components/profile-section/profile-section.component';
+import { LeftSectionComponent } from './header-components/left-section/left-section.component';
 
 
 @NgModule({
-  declarations: [SideBarComponent, HeaderComponent, MenuComponent, ProfileSectionComponent],
+  declarations: [SideBarComponent, HeaderComponent, MenuComponent, ProfileSectionComponent, LeftSectionComponent],
   imports: [
     CommonModule,
     RouterModule,
