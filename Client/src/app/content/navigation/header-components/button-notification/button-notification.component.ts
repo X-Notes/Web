067cf7f1852bd5ba3f4b4acba9abdcb87369a589
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-10-23 23:11:39 +0300

    init notification

--- Client/src/app/content/navigation/header-components/button-notification/button-notification.component.ts
+++ Client/src/app/content/navigation/header-components/button-notification/button-notification.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { AppStore } from 'src/app/core/stateApp/app-state';
+import { UserStore } from 'src/app/core/stateUser/user-state';
+import { Theme } from 'src/app/shared/enums/Theme';
 
 @Component({
   selector: 'app-button-notification',
@@ -6,6 +11,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button-notification.component.scss']
 })
 export class ButtonNotificationComponent implements OnInit {
+
+  @Select(AppStore.getMenuActive)
+  public menuActive$: Observable<boolean>;
+
+  @Select(UserStore.getUserTheme)
+  public theme$: Observable<Theme>;
+  theme = Theme;
+
 
   constructor() { }
 
