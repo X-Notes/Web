d0a5d617137371afe5e108733e19fa141cea96e0
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-10-23 22:13:45 +0300

    add question button

--- Client/src/app/content/navigation/header-components/button-question/button-question.component.ts
+++ Client/src/app/content/navigation/header-components/button-question/button-question.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { AppStore } from 'src/app/core/stateApp/app-state';
+import { UserStore } from 'src/app/core/stateUser/user-state';
+import { Theme } from 'src/app/shared/enums/Theme';
 
 @Component({
   selector: 'app-button-question',
@@ -6,6 +11,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button-question.component.scss']
 })
 export class ButtonQuestionComponent implements OnInit {
+
+
+  @Select(AppStore.getMenuActive)
+  public menuActive$: Observable<boolean>;
+
+  @Select(UserStore.getUserTheme)
+  public theme$: Observable<Theme>;
+  theme = Theme;
 
   constructor() { }
 
