6f8a4f75f4da9d5af0d1856883ae096b2a0379e2
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-10-23 21:32:08 +0300

    theme to component

--- Client/src/app/content/navigation/header-components/button-theme/button-theme.component.ts
+++ Client/src/app/content/navigation/header-components/button-theme/button-theme.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { AppStore } from 'src/app/core/stateApp/app-state';
+import { ChangeTheme } from 'src/app/core/stateUser/user-action';
+import { UserStore } from 'src/app/core/stateUser/user-state';
+import { Theme } from 'src/app/shared/enums/Theme';
 
 @Component({
   selector: 'app-button-theme',
@@ -7,9 +13,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonThemeComponent implements OnInit {
 
-  constructor() { }
+
+  @Select(AppStore.getMenuActive)
+  public menuActive$: Observable<boolean>;
+
+  @Select(UserStore.getUserTheme)
+  public theme$: Observable<Theme>;
+  theme = Theme;
+
+  constructor(private store: Store, ) { }
 
   ngOnInit(): void {
+  }
+
+  toggleTheme() {
+    this.store.dispatch(new ChangeTheme());
   }
 
 }
