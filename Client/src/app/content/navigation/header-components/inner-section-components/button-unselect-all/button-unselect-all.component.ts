4994183e221f564e2eca7a0511600a7e8d34ba1c
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-10-24 18:40:21 +0300

    unselect button to component

--- Client/src/app/content/navigation/header-components/inner-section-components/button-unselect-all/button-unselect-all.component.ts
+++ Client/src/app/content/navigation/header-components/inner-section-components/button-unselect-all/button-unselect-all.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { UnSelectAllFolder } from 'src/app/content/folders/state/folders-actions';
+import { FolderStore } from 'src/app/content/folders/state/folders-state';
+import { UnSelectAllNote } from 'src/app/content/notes/state/notes-actions';
+import { NoteStore } from 'src/app/content/notes/state/notes-state';
+import { AppStore } from 'src/app/core/stateApp/app-state';
+import { UserStore } from 'src/app/core/stateUser/user-state';
+import { Theme } from 'src/app/shared/enums/Theme';
 
 @Component({
   selector: 'app-button-unselect-all',
   templateUrl: './button-unselect-all.component.html',
   styleUrls: ['./button-unselect-all.component.scss']
 })
-export class ButtonUnselectAllComponent implements OnInit {
+export class ButtonUnselectAllComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  destroy = new Subject<void>();
+
+  @Select(UserStore.getUserTheme)
+  public theme$: Observable<Theme>;
+  theme = Theme;
+
+  @Select(AppStore.getMenuActive)
+  public menuActive$: Observable<boolean>;
+
+  public countSelected: number;
+
+  constructor(private store: Store) { }
+
+  ngOnDestroy(): void {
+    this.destroy.next();
+    this.destroy.complete();
+  }
 
   ngOnInit(): void {
+    this.store.select(NoteStore.selectedCount)
+    .pipe(takeUntil(this.destroy))
+    .subscribe(x => {
+      if (x > 0) {
+        this.countSelected = x;
+      }
+    });
+
+    this.store.select(FolderStore.selectedCount)
+    .pipe(takeUntil(this.destroy))
+    .subscribe(x => {
+      if (x > 0) {
+        this.countSelected = x;
+      }
+    });
+
+  }
+
+  unselectAll() {
+    let routePath = this.store.selectSnapshot(AppStore.isNote);
+    if (routePath) {
+      this.store.dispatch(new UnSelectAllNote());
+    }
+    routePath = this.store.selectSnapshot(AppStore.isFolder);
+    if (routePath) {
+      this.store.dispatch(new UnSelectAllFolder());
+    }
   }
 
 }
