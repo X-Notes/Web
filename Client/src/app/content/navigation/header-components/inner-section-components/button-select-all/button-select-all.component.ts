496f4470464a17ee1ac2485458a57aa4fa1c9748
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-10-24 19:05:03 +0300

    button select all to component

--- Client/src/app/content/navigation/header-components/inner-section-components/button-select-all/button-select-all.component.ts
+++ Client/src/app/content/navigation/header-components/inner-section-components/button-select-all/button-select-all.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { SelectAllFolder } from 'src/app/content/folders/state/folders-actions';
+import { SelectAllNote } from 'src/app/content/notes/state/notes-actions';
+import { AppStore } from 'src/app/core/stateApp/app-state';
+import { UserStore } from 'src/app/core/stateUser/user-state';
+import { Theme } from 'src/app/shared/enums/Theme';
 
 @Component({
   selector: 'app-button-select-all',
@@ -7,9 +14,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonSelectAllComponent implements OnInit {
 
-  constructor() { }
+  @Select(AppStore.getMenuActive)
+  public menuActive$: Observable<boolean>;
+
+  @Select(UserStore.getUserTheme)
+  public theme$: Observable<Theme>;
+  theme = Theme;
+
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
+  }
+
+  selectAll() {
+    let routePath = this.store.selectSnapshot(AppStore.isNote);
+    if (routePath) {
+      const noteType = this.store.selectSnapshot(AppStore.getRouting);
+      this.store.dispatch(new SelectAllNote(noteType));
+    }
+    routePath = this.store.selectSnapshot(AppStore.isFolder);
+    if (routePath) {
+      const folderType = this.store.selectSnapshot(AppStore.getRouting);
+      this.store.dispatch(new SelectAllFolder(folderType));
+    }
   }
 
 }
