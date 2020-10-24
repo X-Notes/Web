18196ceb04fc38ed8520a958c44e2ec5be481c50
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-10-24 22:32:19 +0300

    add share and label button to components

--- Client/src/app/content/navigation/header-components/inner-note-section-components/button-label/button-label.component.ts
+++ Client/src/app/content/navigation/header-components/inner-note-section-components/button-label/button-label.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { UserStore } from 'src/app/core/stateUser/user-state';
+import { Theme } from 'src/app/shared/enums/Theme';
 
 @Component({
   selector: 'app-button-label',
@@ -6,6 +10,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button-label.component.scss']
 })
 export class ButtonLabelComponent implements OnInit {
+
+  @Select(UserStore.getUserTheme)
+  public theme$: Observable<Theme>;
+  theme = Theme;
 
   constructor() { }
 
