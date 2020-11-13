977d87dda161197fcf3859224f7a0b1065996195
Konoplyanka Dima <konoplyanka1999@gmail.com>
2020-11-13 19:37:20 +0200

    fix tooltip

--- Client/src/app/shared/tooltip/tooltip.directive.ts
+++ Client/src/app/shared/tooltip/tooltip.directive.ts
@@ -1,6 +1,6 @@
 import { Overlay, OverlayConfig, OverlayPositionBuilder, OverlayRef } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
-import { ComponentRef, Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
+import { ComponentRef, Directive, ElementRef, HostListener, Input, OnDestroy, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TooltipComponent } from './tooltip.component';
 
@@ -8,7 +8,7 @@ import { TooltipComponent } from './tooltip.component';
   // tslint:disable-next-line:directive-selector
   selector: '[CustomTooltip]'
 })
-export class TooltipDirective implements OnInit {
+export class TooltipDirective implements OnDestroy {
 
   @Input('CustomTooltip') text = '';
   // tslint:disable-next-line:no-input-rename
@@ -24,12 +24,8 @@ export class TooltipDirective implements OnInit {
               private overlayPositionBuilder: OverlayPositionBuilder,
               private elementRef: ElementRef) { }
 
-  ngOnInit(): void {
-    const config = new OverlayConfig({
-      positionStrategy: this.positioning(),
-    });
-
-    this.overlayRef = this.overlay.create(config);
+  ngOnDestroy(): void {
+    this.overlayRef?.detach();
   }
 
   positioning() {
@@ -89,12 +85,15 @@ export class TooltipDirective implements OnInit {
   show() {
     if (this.disable === 'true') {
       return;
+    } else if (this.labelColor !== '' && this.text.length < 9) {
+      return;
     }
-    if (this.labelColor !== '') {
-      if (this.text.length < 9) {
-        return;
-      }
-    }
+
+    const config = new OverlayConfig({
+      positionStrategy: this.positioning(),
+    });
+    this.overlayRef = this.overlay.create(config);
+
     const tooltipRef: ComponentRef<TooltipComponent>
       = this.overlayRef.attach(new ComponentPortal(TooltipComponent));
     this.text = this.text.charAt(0).toUpperCase() + this.text.slice(1);
@@ -110,7 +109,7 @@ export class TooltipDirective implements OnInit {
 
   @HostListener('mouseleave')
   hide() {
-    this.overlayRef.detach();
+    this.overlayRef?.detach();
   }
 
 }
