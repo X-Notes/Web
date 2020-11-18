a1852f9855d5abdbad1cf74e7c60f8cf9ed14127
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-11-18 22:26:25 +0200

    fix

--- Backend/WriteAPI/WriteAPI/Controllers/NoteController.cs
+++ Backend/WriteAPI/WriteAPI/Controllers/NoteController.cs
@@ -87,19 +87,19 @@ namespace WriteAPI.Controllers
 
 
         [HttpPatch("label/add")]
-        public async Task<int> AddLabel([FromBody]AddLabelOnNoteCommand command)
+        public async Task AddLabel([FromBody]AddLabelOnNoteCommand command)
         {
             var email = this.GetUserEmail();
             command.Email = email;
-            return await this._mediator.Send(command);
+            await this._mediator.Send(command);
         }
 
         [HttpPatch("label/remove")]
-        public async Task<int> RemoveLabel([FromBody]RemoveLabelFromNoteCommand command)
+        public async Task RemoveLabel([FromBody]RemoveLabelFromNoteCommand command)
         {
             var email = this.GetUserEmail();
             command.Email = email;
-            return await this._mediator.Send(command);
+            await this._mediator.Send(command);
         }
 
 
