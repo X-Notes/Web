578ff7a6412fdc8e9c5b9b393c501c8d0236c42e
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-11-25 15:21:05 +0200

    fix label

--- Client/src/app/shared/pipes/replace.pipe.ts
+++ Client/src/app/shared/pipes/replace.pipe.ts
@@ -6,7 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ReplacePipe implements PipeTransform {
 
   transform(value: string, ...args: any[]): any {
-    return value?.length === 0 ? 'unknown label' : value;
+    if (value) {
+      return value?.length === 0 ? 'unknown label' : value;
+    } else {
+      return 'unknown label';
+    }
   }
 
 }
