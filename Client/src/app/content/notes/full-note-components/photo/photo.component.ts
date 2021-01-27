661d04ee40708b8f6331ec74051608292cf89dab
Davyd Konopatskyi <davydqq@gmail.com>
2021-01-28 01:55:40 +0200

    add basic animation for photos

--- Client/src/app/content/notes/full-note-components/photo/photo.component.ts
+++ Client/src/app/content/notes/full-note-components/photo/photo.component.ts
@@ -1,10 +1,25 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { Photo } from '../../models/ContentMode';
 
 @Component({
   selector: 'app-photo',
   templateUrl: './photo.component.html',
-  styleUrls: ['./photo.component.scss']
+  styleUrls: ['./photo.component.scss'],
+  animations: [
+    trigger('photoInit',
+    [
+      state('noLoaded', style({
+        opacity: 0,
+      })),
+      state('loaded', style({
+        opacity: 1,
+      })),
+      transition('noLoaded => loaded', [
+        animate('0.35s ease-out')
+      ]),
+    ])
+  ]
 })
 export class PhotoComponent implements OnInit {
 
@@ -18,6 +33,7 @@ export class PhotoComponent implements OnInit {
 
   onLoadImage($event)
   {
+    this.photo.loaded = true;
     console.log($event);
   }
 
