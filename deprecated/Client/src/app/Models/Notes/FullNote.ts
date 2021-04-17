e6f2b9a079fbfc0b7e0c89b5287ef5134756f385
Konoplyanka Dima <konoplyanka1999@gmail.com>
2021-04-17 03:43:55 +0300

    delete deprecated project

--- deprecated/Client/src/app/Models/Notes/FullNote.ts
+++
@@ -1,9 +0,0 @@
-import { Label } from '../Labels/Label';
-
-export interface FullNote {
-    Id: string;
-    Title: string;
-    InnerHTML: string;
-    Labels: Label[];
-    Locked: boolean;
-}
--- deprecated/Client/src/app/Services/token-intercepter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TokenIntercepter } from './token-intercepter';
-
-describe('TokenIntercepter', () => {
-  it('should create an instance', () => {
-    expect(new TokenIntercepter()).toBeTruthy();
-  });
-});
