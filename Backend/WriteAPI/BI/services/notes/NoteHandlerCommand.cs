992620c85cae9f9b651e5954b2c859ccdbf1cd37
Davyd Konopatskyi <davydqq@gmail.com>
2021-05-08 20:11:50 +0300

    fix bug

--- Backend/WriteAPI/BI/services/notes/NoteHandlerCommand.cs
+++ Backend/WriteAPI/BI/services/notes/NoteHandlerCommand.cs
@@ -295,7 +295,7 @@ namespace BI.services.notes
             }
 
             var user = await userRepository.FirstOrDefault(x => x.Email == request.Email);
-            var dbNotes = await noteRepository.GetNotesByUserIdAndTypeIdWithContent(user.Id, type.Id);
+            var dbNotes = await noteRepository.GetNotesByUserIdAndTypeIdWithContent(user.Id, type.Id, true);
 
             var orders = Enumerable.Range(1, dbNotes.Count);
             dbNotes = dbNotes.Zip(orders, (note, order) => {
