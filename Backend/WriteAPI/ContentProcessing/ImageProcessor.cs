2f27116032e2f5a88124ddb402891ae9430d3713
Davyd Konopatskyi <davydqq@gmail.com>
2021-06-05 16:48:32 +0300

    init swagger + image processor lib

---
+++ Backend/WriteAPI/ContentProcessing/ImageProcessor.cs
@@ -0,0 +1,31 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentProcessing
+{
+    public class ImageProcessor : IImageProcessor
+    {
+        public async Task Test()
+        {
+            using (Image image = Image.Load("foo.jpg"))
+            {
+                // Resize the image in place and return it for chaining.
+                // 'x' signifies the current image processing context.
+                var clone = image.Clone(x => x.Resize(image.Width / 2, image.Height / 2));
+
+                // The library automatically picks an encoder based on the file extension then
+                // encodes and write the data to disk.
+                // You can optionally set the encoder to choose.
+                image.Save("bar.jpg");
+            } // Dispose - releasing memory into a memory pool ready for the next image you wish to process.
+        }
+
+    }
+}
--- Backend/WriteAPI/WriteAPI/Startup.cs
+++ Backend/WriteAPI/WriteAPI/Startup.cs
@@ -70,6 +70,8 @@ namespace WriteAPI
             services.AddHostedService<MLHosted>();
 
             services.AddHttpClient();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -89,6 +91,16 @@ namespace WriteAPI
             app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseRouting();
             app.UseCors("MyPolicy");
