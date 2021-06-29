924de9450d8dff70e8b10185fa4e67d9f618c49e
Davyd Konopatskyi <davydqq@gmail.com>
2021-06-30 01:28:55 +0300

    restore file

---
+++ Backend/WriteAPI/Domain/Queries/Notes/GetNotesByTypeQuery.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Common.DatabaseModels.Models.Notes;
+using Common.DTO.Notes;
+using Common.DTO.Personalization;
+using MediatR;
+
+namespace Domain.Queries.Notes
+{
+    public class GetNotesByTypeQuery : BaseQueryEntity, IRequest<List<SmallNote>>
+    {
+        [Required]
+        public NoteTypeENUM TypeId { set; get; }
+
+        public PersonalizationSettingDTO Settings { set; get; }
+
+        public GetNotesByTypeQuery(string email, NoteTypeENUM id, PersonalizationSettingDTO settings)
+            : base(email)
+        {
+            this.TypeId = id;
+            this.Settings = settings;
+        }
+    }
+}
