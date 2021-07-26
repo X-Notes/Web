3d89e74ae48b1de9507229cefa8e1740aaa761ac
Davyd Konopatskyi <davydqq@gmail.com>
2021-07-26 21:31:33 +0300

    init full folder note route

--- Client/src/app/content/folders/folders.module.ts
+++ Client/src/app/content/folders/folders.module.ts
@@ -10,6 +10,7 @@ import { SharedComponent } from './shared/shared.component';
 import { DeletedComponent } from './deleted/deleted.component';
 import { ArchiveComponent } from './archive/archive.component';
 import { FullFolderComponent } from './full-folder/full-folder.component';
+import { FullFolderNoteComponent } from './full-folder-note/full-folder-note.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { FullFolderComponent } from './full-folder/full-folder.component';
     DeletedComponent,
     ArchiveComponent,
     FullFolderComponent,
+    FullFolderNoteComponent,
   ],
   imports: [CommonModule, FoldersRouting, SharedModule],
   providers: [MurriService],
---
+++ Client/src/app/content/folders/full-folder-note/full-folder-note.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-full-folder-note',
+  templateUrl: './full-folder-note.component.html',
+  styleUrls: ['./full-folder-note.component.scss']
+})
+export class FullFolderNoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
