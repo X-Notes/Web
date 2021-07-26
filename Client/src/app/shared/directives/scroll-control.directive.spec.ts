a1ffcae912a0a5a4c206518af7d2322c6ea058fc
Davyd Konopatskyi <davydqq@gmail.com>
2021-07-26 22:48:44 +0300

    remove last spec

--- Client/src/app/shared/directives/scroll-control.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ScrollControlDirective } from './scroll-control.directive';
-
-describe('ScrollControlDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ScrollControlDirective();
-    expect(directive).toBeTruthy();
-  });
-});
