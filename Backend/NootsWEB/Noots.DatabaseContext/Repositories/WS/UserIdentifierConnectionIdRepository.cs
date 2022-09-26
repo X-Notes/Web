b90bed16e8055d5a67ebac07997ee86ad0eac123
No lifer <davydqq@gmail.com>
2022-09-27 01:57:57 +0300

    db

--- Backend/NootsWEB/Noots.DatabaseContext/Repositories/WS/UserIdentifierConnectionIdRepository.cs
+++ Backend/NootsWEB/Noots.DatabaseContext/Repositories/WS/UserIdentifierConnectionIdRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Common.DatabaseModels.Models.WS;
+using Microsoft.EntityFrameworkCore;
 using Noots.DatabaseContext.GenericRepositories;
 
 namespace Noots.DatabaseContext.Repositories.WS;
@@ -7,5 +8,11 @@ public class UserIdentifierConnectionIdRepository : Repository<UserIdentifierCon
 {
     public UserIdentifierConnectionIdRepository(NootsDBContext contextDB) : base(contextDB)
     {
+    }
+
+    public Task<List<string>> GetConnectionsAsync(List<Guid> userIds, Guid exceptUserId)
+    {
+        return entities.Where(x => x.UserId != null && x.UserId != exceptUserId && userIds.Contains(x.UserId.Value))
+                       .Select(x => x.ConnectionId).ToListAsync();
     }
 }
