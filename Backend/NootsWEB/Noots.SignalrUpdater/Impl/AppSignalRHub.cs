3e35a8d00b4f5eab867b3431b90a943726ce60ad
Davyd Konopatskyi <dkonopatskyi@deloitte.com>
2022-09-27 15:52:38 +0300

    fix build

--- Backend/NootsWEB/Noots.SignalrUpdater/Impl/AppSignalRHub.cs
+++ Backend/NootsWEB/Noots.SignalrUpdater/Impl/AppSignalRHub.cs
@@ -1,9 +1,7 @@
 ﻿using Common;
 using Common.DatabaseModels.Models.WS;
 using Common.DTO.Parts;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Connections.Features;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using Noots.DatabaseContext.Repositories.WS;
