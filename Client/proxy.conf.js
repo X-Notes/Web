6435ed7b95ea6f5e463743f7957b366f50f47ee2
Konoplyanka <konoplyanka1999@gmail.com>
2022-10-24 01:22:24 +0300

    feat: Add proxy to admin panel

--- Client/proxy.conf.js
+++ Client/proxy.conf.js
@@ -6,7 +6,15 @@ const PROXY_CONFIG = [
       target: "http://localhost:5000",
       secure: false,
       ws: true
-  }
+  },
+  {
+    context: [
+        "/admin"
+    ],
+    target: "http://localhost:3333",
+    secure: false,
+    ws: false
+}
 ]
 
 module.exports = PROXY_CONFIG;
