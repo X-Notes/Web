ae718df6234d9d7ed3ffc9bc0ca168837982b1bf
Davyd Konopatskyi <davydqq@gmail.com>
2023-05-21 21:19:09 +0300

    copying note text menu item

--- Client/src/app/content/notes/api-browser-text.service.ts
+++ Client/src/app/content/notes/api-browser-text.service.ts
@@ -42,24 +42,34 @@ export class ApiBrowserTextService {
     if (navigator.clipboard && window.isSecureContext) {
       await navigator.clipboard.writeText(textToCopy);
     } else {
-      // Use the 'out of viewport hidden text area' trick
-      const textArea = document.createElement('textarea');
+      this.copyTextThroughTextArea(textToCopy);
+    }
+  }
+
+  copyTextThroughTextArea(textToCopy: string, isText: boolean = true): void {
+    // Use the 'out of viewport hidden text area' trick
+    const textArea = document.createElement('textarea');
+
+    if (isText) {
       textArea.value = textToCopy;
+    } else {
+      textArea.innerHTML = textToCopy;
+    }
 
-      // Move textarea out of the viewport so it's not visible
-      textArea.style.position = 'absolute';
-      textArea.style.left = '-999999px';
+    // Move textarea out of the viewport so it's not visible
+    textArea.style.position = 'absolute';
+    textArea.style.left = '-999999px';
 
-      document.body.prepend(textArea);
-      textArea.select();
+    document.body.prepend(textArea);
+    textArea.select();
 
-      try {
-        document.execCommand('copy');
-      } catch (error) {
-        console.error(error);
-      } finally {
-        textArea.remove();
-      }
+    try {
+      document.execCommand('copy');
+      console.log(1);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      textArea.remove();
     }
   }
 
