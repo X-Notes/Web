0a2220d0cdc6a17949acce09dad58e2884b0d0b4
Davyd <davydqq@gmail.com>
2023-11-24 22:33:21 +0200

    remove health check

--- Backend/HealthCheckerWEB/DatabaseContext/Models/UserRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace DatabaseContext.Models
-{
-    public class UserRole : IdentityUserRole<int>
-    {
-
-    }
-}
--- Backend/HealthCheckerWEB/HealthCheckWEB/Models/Azure/AzureConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HealthCheckWEB.Models.Azure
-{
-    public class AzureConfig
-    {
-        public string StorageConnection { set; get; }
-
-        public string StorageUrl { set; get; }
-    }
-}
