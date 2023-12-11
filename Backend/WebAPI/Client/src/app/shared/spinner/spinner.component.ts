d0809e6ff46d02df8f48a16f503bc99958fe3c15
Davyd <davydqq@gmail.com>
2023-12-12 01:06:22 +0200

    spinner dynamic color && select all text

--- Backend/WebAPI/Client/src/app/shared/spinner/spinner.component.ts
+++ Backend/WebAPI/Client/src/app/shared/spinner/spinner.component.ts
@@ -1,9 +1,10 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { ShortUser } from 'src/app/core/models/user/short-user.model';
 import { UserStore } from 'src/app/core/stateUser/user-state';
 import { ThemeENUM } from '../enums/theme.enum';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-spinner',
@@ -17,13 +18,20 @@ export class SpinnerComponent {
   @Select(UserStore.getUserTheme)
   public theme$?: Observable<ThemeENUM>;
 
-  theme = ThemeENUM;
-
   @Input() size?: number;
 
   @Input() isBackground = true;
 
-  @HostBinding('style.--target-color')
   @Input()
   color?: string;
+
+  get spinnerColor$(): Observable<any> {
+    return this.theme$.pipe(map(theme => {
+      if (this.color) {
+        return { 'border': `2px solid ${this.color}`, 'border-bottom-color': 'transparent' }
+      }
+      const border = theme === ThemeENUM.Dark ? '2px solid #ffffff' : '2px solid rgb(134 134 134)';
+      return { 'border': border, 'border-bottom-color': 'transparent' }
+    }));
+  }
 }
